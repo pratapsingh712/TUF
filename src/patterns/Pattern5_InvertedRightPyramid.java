e43c0bbf5ef8fe46c28200a15cc77707d1594736
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-01-10 09:30:20 +0530

    added Pattern5 Pyramid logical

--- src/patterns/Pattern5_InvertedRightPyramid.java
+++ src/patterns/Pattern5_InvertedRightPyramid.java
@@ -4,8 +4,17 @@ public class Pattern5_InvertedRightPyramid {
 
     public static void main(String[] args) {
 
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=6-i;j++){
+//        for(int i=1;i<=5;i++){
+//            for(int j=1;j<=6-i;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        // let's try to find something more logical and not hardcoded for inner loop
+
+        for(int i=5;i>=1;i--){
+            for(int j=i;j>=1;j--){
                 System.out.print("* ");
             }
             System.out.println();
