76d00ad6045baeebf3d4b13ec9b0b4ef7bed78f8
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-01-10 19:15:14 +0530

    added pattern 14 Letter Triangle

---
+++ src/patterns/Pattern14_IncreasingLetterTrianglePattern.java
@@ -0,0 +1,14 @@
+package patterns;
+
+public class Pattern14_IncreasingLetterTrianglePattern {
+
+    public static void main(String[] args) {
+
+        for(char i ='A'; i <= 'E'; i++){
+            for(char j = 'A'; j <= i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
