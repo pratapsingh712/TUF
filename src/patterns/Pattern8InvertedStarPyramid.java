42899609192dda30fffb5589b36adbb4859fab38
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-01-10 10:13:41 +0530

    added pattern 8 inverted star pyramid

---
+++ src/patterns/Pattern8InvertedStarPyramid.java
@@ -0,0 +1,17 @@
+package patterns;
+
+public class Pattern8InvertedStarPyramid {
+
+    public static void main(String[] args) {
+
+        for(int i=5;i>=1;i--){
+            for(int j=5;j>i-1;j--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
