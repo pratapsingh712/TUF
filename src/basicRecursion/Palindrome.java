e15442cdd02e3d674b56bcd6f72285f6791a7644
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-01-11 22:41:54 +0530

    implemented Optimal solution

--- src/basicRecursion/Palindrome.java
+++ src/basicRecursion/Palindrome.java
@@ -3,29 +3,50 @@ package basicRecursion;
 public class Palindrome {
     public static void main(String[] args) {
 
-        String str = "moma";
+        String str = "mom";
 
         String result = "";
 
-        String rev = reverse(str,result,str.length()-1);
-
-        if(rev.equals(str)){
-            System.out.println("Palindrome");
-        }
-        else{
+        if(reverse(str,0,str.length()-1)){
+            System.out.println("Palindrome:");
+        }else{
             System.out.println("Not a Palindrome:");
         }
+
+//        String rev = reverse(str,result,str.length()-1);
+//
+//        if(rev.equals(str)){
+//            System.out.println("Palindrome");
+//        }
+//        else{
+//            System.out.println("Not a Palindrome:");
+//        }
+//    }
+//
+//    public static String reverse(String str,String result, int rightIndex){
+//        if(rightIndex<0){
+//            return result;
+//        }
+//
+//        char temp = str.charAt(rightIndex);
+//        result = result+temp;
+//        rightIndex--;
+//
+//        return reverse(str,result,rightIndex);
+//    }
     }
 
-    public static String reverse(String str,String result, int rightIndex){
-        if(rightIndex<0){
-            return result;
+    // alright there is another optimal solution
+
+    public static boolean reverse(String str, int left, int right){
+        if(left>=right){
+            return true;
         }
 
-        char temp = str.charAt(rightIndex);
-        result = result+temp;
-        rightIndex--;
+        if(str.charAt(left)!=str.charAt(right)){
+            return false;
+        }
 
-        return reverse(str,result,rightIndex);
+        return reverse(str,left+1,right-1);
     }
 }
