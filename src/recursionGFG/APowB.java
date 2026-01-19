c35e1f429ddd6cc924c172383f3714fe030f27e0
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-01-19 22:19:58 +0530

    added A pow B using Log function

--- src/recursionGFG/APowB.java
+++ src/recursionGFG/APowB.java
@@ -10,7 +10,18 @@ public class APowB {
         System.out.println("Enter the power :");
         int b = sc.nextInt();
 
-        System.out.println(a+" power "+b+" is "+pow(a,b));
+
+        // we can also do the operation in log time using a^b = a^b/2*a^b/2;
+
+        System.out.println(a+" power "+b+" is "+logPow(a,b));
+    }
+
+    private static int logPow(int a, int b){
+        if(b==0) return 1;
+
+        int fun = logPow(a,b/2);
+        if(b%2==0) return fun*fun;
+        else return a*fun*fun;
     }
 
     private static int pow(int a, int b){
