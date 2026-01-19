32c598b35126a7ab17befcb15ae2d6405c862ce2
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-01-19 22:37:49 +0530

    added function to reverse a number using recursion

---
+++ src/recursionGFG/ReverseNumber.java
@@ -0,0 +1,23 @@
+package recursionGFG;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number :");
+        int num = sc.nextInt();
+
+        System.out.println("Original Number is : "+num);
+        System.out.println("After reversing : "+reverse(num,0));
+
+    }
+
+    private static int reverse(int num, int rev) {
+        if(num==1) return rev;
+
+        return reverse(num/10, rev*10+num%10);
+
+    }
+}
