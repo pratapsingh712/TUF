63f33a86f692fc4983647b688bd7a3c726ad8151
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-01-21 17:30:50 +0530

    added optimal solution to move zeros to end with auxilary space

--- src/arrayEasy/MoveZerosToEnd.java
+++ src/arrayEasy/MoveZerosToEnd.java
@@ -9,7 +9,7 @@ public class MoveZerosToEnd {
         System.out.println("Enter the size of array :");
         int size = sc.nextInt();
         int[] array = new int[size];
-        int[] zerosAtEndCopy = new int[size];
+//        int[] zerosAtEndCopy = new int[size];
 
         System.out.println("Enter the elements in your array :");
         for(int i=0;i<size;i++){
@@ -18,22 +18,49 @@ public class MoveZerosToEnd {
 
         System.out.println("Original Array :"+ Arrays.toString(array));
 
-        moveZeros(array,zerosAtEndCopy);
+        optimalMoveZeros(array);
+
+//        moveZeros(array,zerosAtEndCopy);
     }
 
-    private static void moveZeros(int[] array, int[] zerosAtEndCopy) {
-
+    private static void optimalMoveZeros(int[] array) {
         int index = 0;
-        for(int i=0;i<array.length;i++){
-            if(array[i]!=0){
-                zerosAtEndCopy[index++] = array[i];
+
+        for(int j=1;j<array.length;j++){
+            if(array[index]==0 && array[j]!=0){
+                int temp = array[j];
+                array[j] = array[index];
+                array[index] = temp;
+                index++;
+            } else if(array[index]!=0){
+                index++;
             }
         }
 
+        // the work is done
+
         System.out.println("After moving zeros to end :");
-        for(Integer num : zerosAtEndCopy){
+
+        for(Integer num : array){
             System.out.print(num+" ");
         }
         System.out.println();
     }
+
+
+//    private static void moveZeros(int[] array, int[] zerosAtEndCopy) {
+//
+//        int index = 0;
+//        for(int i=0;i<array.length;i++){
+//            if(array[i]!=0){
+//                zerosAtEndCopy[index++] = array[i];
+//            }
+//        }
+//
+//        System.out.println("After moving zeros to end :");
+//        for(Integer num : zerosAtEndCopy){
+//            System.out.print(num+" ");
+//        }
+//        System.out.println();
+//    }
 }
