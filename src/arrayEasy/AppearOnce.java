dadefa15ed49940c4350ce89b6f03873d7cf15b9
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-01-21 20:18:49 +0530

    implemented hashmap or efficient solution

--- src/arrayEasy/AppearOnce.java
+++ src/arrayEasy/AppearOnce.java
@@ -1,6 +1,8 @@
 package arrayEasy;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class AppearOnce {
@@ -21,26 +23,47 @@ public class AppearOnce {
     }
 
     private static void appearedOnce(int[] array) {
-        int count = 0;
-        boolean[] visited = new boolean[array.length];
+        HashMap<Integer,Integer> map = new HashMap<>();
 
         for(int i=0;i<array.length;i++){
-            if(!visited[i]) {
-                count = 0;
-                for (int j = 0; j < array.length; j++) {
-                    if (array[i] == array[j]) {
-                        count++;
-                        visited[j] = true;
-                    }
-                }
+            if(!map.containsKey(array[i])){
+                map.put(array[i],1);
+            }else{
+                map.put(array[i],map.getOrDefault(array[i],0)+1);
             }
+        }
 
-            if(count==1){
-                System.out.println(array[i]+" is appeared only once :");
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue()==1){
+                System.out.println("Value appeared only once in array is :"+entry.getKey());
                 return;
             }
         }
 
-        System.out.println("No element appeared only once :");
+        System.out.println("No value appeared only once :");
     }
+
+//    private static void appearedOnce(int[] array) {
+//        int count = 0;
+//        boolean[] visited = new boolean[array.length];
+//
+//        for(int i=0;i<array.length;i++){
+//            if(!visited[i]) {
+//                count = 0;
+//                for (int j = 0; j < array.length; j++) {
+//                    if (array[i] == array[j]) {
+//                        count++;
+//                        visited[j] = true;
+//                    }
+//                }
+//            }
+//
+//            if(count==1){
+//                System.out.println(array[i]+" is appeared only once :");
+//                return;
+//            }
+//        }
+//
+//        System.out.println("No element appeared only once :");
+//    }
 }
