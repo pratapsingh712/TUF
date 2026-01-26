1675825f2b9aeb9517d5de89123cffef72f63feb
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-01-26 22:11:25 +0530

    added some comment

--- src/binarySearchEasy/SearchInsertPosition.java
+++ src/binarySearchEasy/SearchInsertPosition.java
@@ -28,7 +28,7 @@ public class SearchInsertPosition {
 
         for(int i=0;i< array.length-1;i++){
             if(array[i]<key && array[i+1]>key){
-                position = i+2;
+                position = i+2; // this i am doing so that we can handle to increasing size of array as well and return proper index value where we can insert
                 System.out.println("Position at which you can insert is : "+position);
                 return;
             }
